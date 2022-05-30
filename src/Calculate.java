ed2ee47b7a0b2b78ed5ca746a77ea5d5b907b94c
red-lizard88 <i.liashchanka@gmail.com>
2022-05-30 13:50:41 +0300

    Тест2
    Тест2

---
+++ src/Calculate.java
@@ -0,0 +1,25 @@
+public class Calculate {
+
+
+    public static int summa(int number1, int number2) {
+        int result = number1 + number2;
+        return result;
+    }
+
+    public static int div(int number1, int number2) {
+        int result = number1 / number2;
+        return result;
+    }
+
+    public static int subtraction(int number1, int number2) {
+        int result = number1 - number2;
+        return result;
+    }
+
+    public static int multiplication(int number1, int number2) {
+        int result = number1 * number2;
+        return result;
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -2,54 +2,70 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
-
-
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        int result = 0;
+
         String operation = "";
 
         System.out.println("---- Калькулятор ----");
         System.out.println("Ввод производится по схеме: число 1 + <Enter> + операция + <Enter> + число 2");
         System.out.println("Введите число 1:");
 
+
         try {
             sc.hasNextInt();
             int number1 = sc.nextInt();
 
-            System.out.println("Введите операцию:");
-            operation = sc.next();
+
+            while (!(operation.equals("/")) && !(operation.equals("+")) && !(operation.equals("*")) && !(operation.equals("-"))) {
+                System.out.println("Введите операцию:");
+                operation = sc.next();
+            }
+
+
             System.out.println("Введите число 2:");
 
             try {
-                sc.hasNextInt();
-                int number2 = sc.nextInt();
 
-                switch (operation) {
-                    case "+":
-                        result = number1 + number2;
-                        break;
-                    case "-":
-                        result = number1 - number2;
-                        break;
-                    case "*":
-                        result = number1 * number2;
-                        break;
-                    case "/":
-                        result = number1 / number2;
-                        break;
-                    default:
-                        System.out.println("Операция не распознана. Повторите ввод.");
+                try {
+                    int result;
+                    sc.hasNextInt();
+                    int number2 = sc.nextInt();
+
+
+                    switch (operation) {
+                        case "+":
+                            result = Calculate.summa(number1, number2);
+                            System.out.println("Результат = " + result);
+                            break;
+                        case "-":
+                            result = Calculate.subtraction(number1, number2);
+                            System.out.println("Результат = " + result);
+                            break;
+                        case "*":
+                            result = Calculate.multiplication(number1, number2);
+                            System.out.println("Результат = " + result);
+                            break;
+                        case "/":
+                            result = Calculate.div(number1, number2);
+                            System.out.println("Результат = " + result);
+                            break;
+                        default:
+                            System.out.println("Операция не распознана. Повторите ввод.");
+                    }
+
+
+                } catch (InputMismatchException number2) {
+                    System.out.println("Извините, но это не число. Запустите сначала и введите число.");
                 }
-
-                System.out.println("Результат = " + result);
-
-            } catch (InputMismatchException number2) {
-                System.out.println("Извините, но это не число. Запустите сначала и введите число.");
+            } catch (ArithmeticException result) {
+                System.out.println("Деление на ноль. Запустите сначала и введите число.");
             }
 
-        } catch (InputMismatchException number1) {
+
+        } catch (
+                InputMismatchException number1) {
             System.out.println("Извините, но это не число. Запустите сначала и введите число.");
         }
 
